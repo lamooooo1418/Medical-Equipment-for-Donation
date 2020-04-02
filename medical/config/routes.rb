5b6a1e2d8b05a33ade19ddb8f6a9fda5e0321854
Lamooooo1418 <lamooooo1418@gmail.com>
2020-04-02 23:10:20 +0300

    Edit and Delete Pair Programming

--- medical/config/routes.rb
+++ medical/config/routes.rb
@@ -1,6 +1,9 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do root to: "equipments#index"
   root 'equipments#index'
-  
-  resources :equipments
- 
-end
+  #get 'equipments/:id', to: 'equipments#show', as: 'equipments'
+  resources :equipments 
+  #get 'equipments', to: 'equipments#index'
+  #get "equipments/new", to: "equipments#new"
+  #post "equipments", to: "equipments#create"
+  #get 'equipments/:id', to: 'equipments#show', as: 'equipment'
+   end
