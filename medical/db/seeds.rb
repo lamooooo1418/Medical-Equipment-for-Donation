6c596ae9ddab501e5b2aa5cfa37c2c705f115707
Lamooooo1418 <lamooooo1418@gmail.com>
2020-04-07 19:12:12 +0300

    remove Form

--- medical/db/seeds.rb
+++ medical/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+# Equipment.create(name: "Wheelchair", description:"Medline Lightweight & User-Friendly Wheelchair With Flip-Back, Desk-Length Arms & Elevating Leg Rests for Extra Comfort, Gray, 18 Seat ", time_of_use: "1 year", phone: "0598765433")
+# Equipment.create(name: "Blood Glucose Monitor", description:"The Accu-Chek Performa blood glucose meter makes testing your blood sugar quick and easy. It's ready to use right out of the box, requiring no set-up. Each meter uses Accu-Chek Performa test strips and an Accu-Chek lancing device. Helpful alerts remind you to test before and after meals, giving you valuable information to support therapy decisions for a healthy active life.", time_of_use: "new", phone: "05444444455")
+# Equipment.create(name: "Blood Pressure Monitor", description:"Helps to diagnose and monitor hypertension Can help detect your cardiovascular risk A more complete picture of your heart health", time_of_use: "2 years", phone: "0598712345")
+
+# Hospital.create(hospital_name:"Hayat national hospital riyadh", equipment_name: "Wheelchair", quantitity:"3", phone: "0598765433")
+# Hospital.create(hospital_name:"Specialized Medical Center Hospital", equipment_name: "Blood Glucose Monitor", quantitity:"5", phone: "05444444455")
+# Hospital.create(hospital_name:"King Fahad National Guard Hospital ",equipment_name: "Blood Pressure Monitor", quantitity:"24", phone: "0598712345")
